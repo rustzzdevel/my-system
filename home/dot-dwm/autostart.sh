44b5608ed5697d8afbcc0829a8d75b331d15aeda
qqrustzz@gmail.com <unconfigured@null.spigotmc.org>
2023-11-11 13:42:22 +0300

    upload

---
+++ home/dot-dwm/autostart.sh
@@ -0,0 +1,9 @@
+xcompmgr &
+setxkbmap -layout "us,ru" -option "grp:alt_shift_toggle" &
+
+feh --bg-scale ~/.wp.png &
+
+copyq --start-server &
+dunst &
+easyeffects --gapplication-service &
+flameshot &
