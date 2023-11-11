3c457cecf67805bc4fae61d1bf02c4821342f77c
rustzzdevel <qqrustzz@gmail.com>
2023-11-11 21:02:47 +0300

    update & add

--- home/dot-dwm/autostart.sh
+++ home/dot-dwm/autostart.sh
@@ -1,9 +1,13 @@
 xcompmgr &
-setxkbmap -layout "us,ru" -option "grp:alt_shift_toggle" &
-
 feh --bg-scale ~/.wp.png &
+xrdb -merge ~/.Xresources &
 
 copyq --start-server &
 dunst &
 easyeffects --gapplication-service &
 flameshot &
+
+setxkbmap -layout "us,ru" -option "grp:alt_shift_toggle" &
+pipewire &
+pipewire-pulse &
+wireplumber &
