fd666166e8c98df4d1ec3ed7424f1bfbca205377
rustzzdevel <qqrustzz@gmail.com>
2023-11-12 21:46:10 +0300

    update structure

---
+++ home/user/.bash_profile
@@ -0,0 +1,9 @@
+#
+# ~/.bash_profile
+#
+
+export PATH="$PATH:$HOME/.flutter-sdk/bin"
+export PATH="$PATH:$HOME/.bin"
+alias startx="startx 2> /dev/null"
+
+[[ -f ~/.bashrc ]] && . ~/.bashrc
---
+++ home/user/.dwm/autostart.sh
@@ -0,0 +1,13 @@
+xcompmgr &
+feh --bg-scale ~/.wp.png &
+
+copyq --start-server &
+dunst &
+easyeffects --gapplication-service &
+flameshot &
+slstatus &
+
+setxkbmap -layout "us,ru" -option "grp:alt_shift_toggle" &
+pipewire &
+pipewire-pulse &
+wireplumber &
